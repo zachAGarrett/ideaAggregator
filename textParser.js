27d9dd3dbbf0414ec663066d204e891e7117b397
zachAGarrett <44583659+zachAGarrett@users.noreply.github.com>
2020-08-10 23:42:27 -0500

    added text parser

--- textParser.js
+++ textParser.js
@@ -1,22 +1,25 @@
 const fs = require('fs');
-const text_path = `${__dirname}/data/textBody`;
+
+// we can modify the entrypoint for a body of text
+const file = '/data/textBody';
+const text_path = `${__dirname}${file}`;
 
 const text = fs.readFileSync(text_path, 'utf8', (err, data) => {
     console.warn(err);
     return data;
 });
-// console.log(text);
+
+// this function will remove periods after common titles
 const serialize = (textString) => {
     const regex_titles = /(?<=(?<titles>(mrs)|(mr)|(dr)|(esq)|(hon)|(jr)|(ms)|(messrs)|(mmes)|(msgr)|(prof)|(rev)|(rt)|(sr)|(st)))\./gi;
-    // const regex_abbr = /(?<acronyms>(\w\.){2,})/g;
     const t = textString.replace(regex_titles, '');
     return t;
 };
-const serialized = serialize(text);
-// console.log(serialized);
+
+// this will match sentences, but fail on acronyms
 const regex_sentences = /(?<sentences>[^.?!]+[.?!])/g;
-const sentences = serialized.match(regex_sentences);
-// console.log(sentences);
+
+// this function will concatenate sentences which were unintentionally broken by acronyms, and then return all sentences
 const concatSentences = (arr) => {
     const checked = [];
     for (let i = 0; i < arr.length; i += 1) {
@@ -41,5 +44,7 @@ const concatSentences = (arr) => {
     }
     return checked;
 };
-console.log(concatSentences(sentences));
-console.log(concatSentences(sentences));
+
+// test
+console.log( concatSentences( serialize(text).match(regex_sentences) ) );
+
